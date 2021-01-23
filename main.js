2221e6c032ca8ede0615dd324fdebbb27bdbd0fc
Tad Sekeres <twsekeres@gmail.com>
2021-01-23 10:57:19 -0600

    tested selectors

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("Ho Ender.");
+
+// const button = document.getElementById('main-btn');
+
+// console.log(button);
+
+// const qbutton = document.querySelector('#main-btn');
+
+// console.log(qbutton);
