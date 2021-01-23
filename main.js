8e9ffd938cbe75d54ba2ed92987659609cdb765a
Tad Sekeres <twsekeres@gmail.com>
2021-01-23 11:09:17 -0600

    try again later

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 console.log("Ho Ender.");
+
+function sayLoveToUser(user) {
+  return `I love ${user}!`;
+}
+
+function sayHateToUser(user) {
+  return `I hate ${user}!`;
+}
+
+function createTeenageGreeting(user, cb) {
+  return cb(user);
+}
+
+console.log(createGreeting('Dad', sayLoveToUser));
+console.log(createGreeting('Dad', sayHateToUser));
