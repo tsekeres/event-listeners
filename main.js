bdfb6c7471630f43b9335d1c6142d62726f81664
Tad Sekeres <twsekeres@gmail.com>
2021-01-31 19:17:59 -0600

    finishing up the code from class

--- main.js
+++ main.js
@@ -70,15 +70,44 @@ const printToDom = (divId, textToPrint) => {
 
 const pieBuilder = (taco) => {
   let domString = '';
-  for (let i = 0; i < taco.length; i++) {
+  // for (let i = 0; i < taco.length; i++) {
+  //   domString += `<div class="card my-2" style="width: 18rem;" id=${i}>
+  //                   <div class="img-container" style="background-image: url('${taco[i].imageUrl}');"></div>
+  //                   <div class="card-body">
+  //                     <p class="card-text">${taco[i].name}</p>
+  //                     <p class="card-text">${taco[i].ingredients}</p>
+  //                     <p class="card-text">${taco[i].bakeTemp}</p>
+  //                     <p class="card-text">${taco[i].drinkPairing}</p>
+  //                     <p class="card-text">${taco[i].iceCream}</p>
+  //                     <button type="button" class="btn btn-danger" id="${i}">Delete</button>
+  //                   </div>
+  //                 </div>`;
+  // }
+
+  // taco.forEach((item, i) => {
+  //   domString += `<div class="card my-2" style="width: 18rem;" id=${i}>
+  //                   <div class="img-container" style="background-image: url('${item.imageUrl}');"></div>
+  //                   <div class="card-body">
+  //                     <p class="card-text">${item.name}</p>
+  //                     <p class="card-text">${item.ingredients}</p>
+  //                     <p class="card-text">${item.bakeTemp}</p>
+  //                     <p class="card-text">${item.drinkPairing}</p>
+  //                     <p class="card-text">${item.iceCream}</p>
+  //                     <button type="button" class="btn btn-danger" id="${i}">Delete</button>
+  //                   </div>
+  //                 </div>`;
+  // })
+
+  // FOR OF LOOP
+  for (let [i, element] of taco.entries()) {
     domString += `<div class="card my-2" style="width: 18rem;" id=${i}>
-                    <div class="img-container" style="background-image: url('${taco[i].imageUrl}');"></div>
+                    <div class="img-container" style="background-image: url('${element.imageUrl}');"></div>
                     <div class="card-body">
-                      <p class="card-text">${taco[i].name}</p>
-                      <p class="card-text">${taco[i].ingredients}</p>
-                      <p class="card-text">${taco[i].bakeTemp}</p>
-                      <p class="card-text">${taco[i].drinkPairing}</p>
-                      <p class="card-text">${taco[i].iceCream}</p>
+                      <p class="card-text">${element.name}</p>
+                      <p class="card-text">${element.ingredients}</p>
+                      <p class="card-text">${element.bakeTemp}</p>
+                      <p class="card-text">${element.drinkPairing}</p>
+                      <p class="card-text">${element.iceCream}</p>
                       <button type="button" class="btn btn-danger" id="${i}">Delete</button>
                     </div>
                   </div>`;
@@ -120,7 +149,53 @@ const handleButtonClick = (e) => {
   } else {
     pieBuilder(selectedPies);
   }
+}
 
+
+// C in CRUD: Create new Pies
+const getFormInfo = (e) => {
+  // Stop the page from refreshing
+  e.preventDefault();
+
+  // Grabbing all the values of the inputs/form fields
+  const name = document.querySelector('#name').value;
+  const ingredients = document.querySelector('#ingredients').value;
+  const bakeTemp = document.querySelector('#bakeTemp').value;
+  const drinkPairing = document.querySelector('#drinkPairing').value;
+  const imageUrl = document.querySelector('#imageUrl').value;
+  const instructor = document.querySelector('#instructor').value;
+  const iceCream = document.querySelector('#iceCream').value;
+
+  // Short hand object notation to add values to obj
+  const obj = {
+    name,
+    bakeTemp,
+    ingredients,
+    drinkPairing,
+    imageUrl,
+    instructor,
+    iceCream,
+  }
+
+  // Pushing the new object up to the pies array
+  pies.push(obj);
+
+  // Rebuilding the DOM
+  pieBuilder(pies);
+
+  document.querySelector('form').reset();
+}
+
+// D in CRUD: Delete the Pie
+const deletePie = (e) => {
+  const targetType = e.target.type;
+  const targetId = e.target.id;
+  if (targetType === 'button') {
+    // DO SOMETHING
+    pies.splice(targetId, 1);
+  } 
+
+  pieBuilder(pies);
 }
 
 const buttonEvents = () => {
@@ -128,6 +203,11 @@ const buttonEvents = () => {
   document.querySelector('#Doc').addEventListener('click', handleButtonClick);
   document.querySelector('#Aja').addEventListener('click', handleButtonClick);
   document.querySelector('#Trinity').addEventListener('click', handleButtonClick);
+  
+  // Targeting the Delete Button
+  document.querySelector('#pies').addEventListener('click', deletePie);
+
+  document.querySelector('form').addEventListener('submit', getFormInfo);
 }
 
 const init = () => {
